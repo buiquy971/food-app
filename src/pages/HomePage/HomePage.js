15f3b45ffc8a1d08429ae6b04e83b2cedecde8b6
buiquy971 <139255535+buiquy971@users.noreply.github.com>
2023-11-06 08:41:09 +0700

    No4

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -477,9 +477,6 @@ function HomePage() {
             </div>
           </div>
         </div>
-        <div>asfjisjdfokasofjkqj3nsidnff
-          fnkasmdfoasdkf0kawoegjiowmnevbkdsm
-        </div>
       </div>
     </div>
   );
